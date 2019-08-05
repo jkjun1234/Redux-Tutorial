a1829bc92728aa9abebc52f40a0d4391017cf22d
jkjun1234 <jkjun1234@gmail.com>
2019-08-05 22:03:17 +0900

    React 초기코드 스타일링 정리

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 - Link
 Flux 카툰 안내서 ( http://bestalign.github.io/2015/10/06/cartoon-guide-to-flux/ )
+
 Redux 카툰 안내서 ( http://bestalign.github.io/2015/10/26/cartoon-intro-to-redux/ )
+
+React 초기코드 스타일링 
+( https://github.com/airbnb/javascript/tree/master/react )
---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react';
+
+const proTypes = {
+
+};
+
+const defaultProps = {
+
+};
+
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+App.PropTypes = PropTypes;
+App.defaultProps = defaultProps;
+
+export default App;
